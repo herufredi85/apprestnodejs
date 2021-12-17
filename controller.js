5730a9a526cf25b4dbe13004d9c9f1c7c5095d98
unknown <social@digidoc.co.id>
2021-12-17 10:25:32 +0700

    tampil data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection =require('./koneksi');
 exports.index=function(eq,res){
     response.ok("rest api running....",res);
 }
+
+exports.tampilsemuamahasiswa=function(eq,res){
+    connection.query("select * from mahasiswa",function(error,rows,fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    });
+   
+}
